fdbbb9577b15853c88815b4b5a7bc4021f1ba22f
Pavel Kamyshov <pkamyshov@gmail.com>
2014-08-04 13:11:10 +0300

    ome commit for the fcirst commit HellOWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Pavlo_Kamyshov on 8/1/2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello WORLD 11111!!");
+    }
+}
+
