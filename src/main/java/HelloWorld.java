a73d1232e66a861f34c0a06525419765983ff052
Pavel Kamyshov <pkamyshov@gmail.com>
2014-08-04 18:20:31 +0300

    updated testing

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello WORLD 11111!!");
+        System.out.println("Hello WORLD testing!!");
     }
 }
 
